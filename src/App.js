dba7b7e6f8d1382c6377df6d4443e4d0c6f79daa
Arnabgi <nehakumari@matrixnmedia.com>
2023-08-23 16:32:06 +0530

    function called

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react"
 
-import User from './User'
+
 const App=() =>{
+  const apple=()=>{
+    alert ("function called");
+  }
   return (
-    <div>
-     <h1>HELLO WORLD</h1>
-     <h6>its me neha</h6>
-          <User />
+    <div className="App">
+      <h1>hello world</h1>
+      <button onClick={apple}>click me</button>
+    
     </div>
   )
 }
