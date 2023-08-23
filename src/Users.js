48a7add9410b602bd7f4bf46b74977df8f05ab4c
Arnabgi <arnabsadhukhan@matrixnmedia.com>
2023-08-23 15:43:53 +0530

    app to user

---
+++ src/Users.js
@@ -0,0 +1,12 @@
+function Users() {
+    return (
+      <div className="App">
+       <h1>HELLOUSERS</h1>
+       <h6>its me neha</h6>
+            
+      </div>
+      );                                                     
+    }
+    
+    export default Users;
+    
